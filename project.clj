76dd52c5268d9d770e93f057e9e2a4f8d3ac03dc
piotr-yuxuan <pierre2b@adikteev.com>
2017-10-13 01:55:48 +0200

    Bump versions

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject max-puzzle "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.495"]
-                 [reagent "0.6.1"]
-                 [re-frame "0.9.2"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [reagent "0.8.0-alpha1"]
+                 [re-frame "0.10.2"]
                  [org.clojure/math.combinatorics "0.1.4"]
-                 [garden "1.3.2"]]
+                 [garden "1.3.3"]]
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :plugins [[lein-cljsbuild "1.1.6-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "1.1.7"]
             [lein-shell "0.5.0"]
             [lein-pdo "0.1.1"]
-            [lein-ancient "0.6.10"]
+            [lein-ancient "0.6.12"]
             [lein-garden "0.3.0"]]
   :min-lein-version "2.5.3"
   :source-paths ["src/clj"]
@@ -35,10 +35,10 @@
                      :stylesheet max-puzzle.css/screen
                      :compiler {:output-to "resources/public/css/screen.css"
                                 :pretty-print? false}}]}
-  :profiles {:dev {:plugins [[lein-figwheel "0.5.9"]]
-                   :dependencies [[binaryage/dirac "1.2.1"]
-                                  [binaryage/devtools "0.9.2"]
-                                  [re-frisk "0.4.4"]]
+  :profiles {:dev {:plugins [[lein-figwheel "0.5.15-SNAPSHOT"]]
+                   :dependencies [[binaryage/dirac "1.2.17"]
+                                  [binaryage/devtools "0.9.7"]
+                                  [re-frisk "0.5.0"]]
                    :repl-options {:port 8230
                                   :nrepl-middleware [dirac.nrepl/middleware]
                                   :init (do
